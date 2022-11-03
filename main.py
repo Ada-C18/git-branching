e5e2bd17cd23cb2246f94612664e1d02b2c4e249
atifaayman <113575857+atifaayman@users.noreply.github.com>
2022-11-03 10:54:08 -0700

    Update main.py
    add a space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #creating a simple function
 def my_function(fname, lname):
-  print(f"My first name is {fname} My last name is{lname}.")
+  print(f"My first name is {fname} My last name is {lname}.")
 
 my_function("Trenisha", "Goslee")
