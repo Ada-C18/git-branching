d9a2f167ce7d3f71644d01a3003fe7406ee712a1
Minh Seikel <99860964+goldenkairos@users.noreply.github.com>
2022-11-03 10:55:43 -0700

    Update main.py
    adding information for more details

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #creating a simple function
 def my_function(fname, lname):
-  print(f"Cheetah's instructor is: {fname} {lname}!")
+  print(f"Cheetah's instructor's first name is {fname} and last name is {lname}!")
 
 my_function("Jasmine", "Lopez")
 
