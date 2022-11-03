281db799a0709927452949a7073c55301327e56e
Minh Seikel <99860964+goldenkairos@users.noreply.github.com>
2022-11-03 10:53:58 -0700

    Update main.py
    adding punctuation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #creating a simple function
 def my_function(fname, lname):
-  print(f"Cheetah's instructor is: {fname} {lname}")
+  print(f"Cheetah's instructor is: {fname} {lname}!")
 
 my_function("Jasmine", "Lopez")
 
