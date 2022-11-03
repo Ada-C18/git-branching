9f6ad63afd9bbce05da672adc2a000498879a3c9
Lynn <88120703+1lynnj@users.noreply.github.com>
2022-11-03 10:39:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #creating a simple function
 def my_function(fname, lname):
-  print(f"My name is {fname} {lname}")
+  print(f"My name is {fname} {lname}.")
 
 my_function("Lynn", "Jansheski")
 my_function("Nancy", "Harris")
